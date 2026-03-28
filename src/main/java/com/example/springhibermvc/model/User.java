fc493360285b6c36a45dd66407bd66f304a636b7
deus <deus2001@mail.ru>
2026-03-28 19:23:36 +0700

    uwu1

--- src/main/java/com/example/springhibermvc/model/User.java
+++ src/main/java/com/example/springhibermvc/model/User.java
@@ -28,38 +28,17 @@ public class User {
         this.email = email;
     }
 
-    // Геттеры и сеттеры
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
-    public String getName() {
-        return name;
-    }
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
 
     @Override
     public String toString() {
